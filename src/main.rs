2df5fef6f5b9001b2228df6b16d6f5ea5a58608d
Tjark Döring <mail@tjark-doering.de>
2025-04-25 14:31:35 +0200

    Backend returns random prompts

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use axum::{
-    Router,
-    routing::{get, post},
-};
+use axum::{Json, Router, routing::get};
+use rusqlite::Connection;
+use serde::Serialize;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
 use tower_http::services::ServeFile;
@@ -12,7 +11,7 @@ async fn main() -> anyhow::Result<()> {
     // pass incoming GET requests on "/hello-world" to "hello_world" handler.
     let app = Router::new()
         .fallback_service(ServeFile::new("index.html"))
-        .route("/frottage", post(frottage_function));
+        .route("/random_prompt", get(random_prompt));
 
     // write address like this to not make typos
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -23,8 +22,62 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn frottage_function(body: String) -> &'static str {
-    println!("Lets frottage in the browser now!");
-    println!("This is the {}", body);
-    "Lets frottage :)"
+#[derive(Serialize)]
+struct Prompt {
+    target: String,
+    prompt: String,
+}
+
+#[derive(Debug)]
+struct DbPrompt {
+    id: i64,
+    prompt: String,
+}
+
+#[derive(Debug)]
+struct DbTarget {
+    name: String,
+    aspectRatio: String,
+}
+
+async fn random_prompt() -> Json<Vec<Prompt>> {
+    println!("Lets frottage!");
+
+    let conn = Connection::open("database.db").unwrap();
+
+    let mut stmt_targets = conn
+        .prepare("select name, aspectRatio from targets")
+        .unwrap();
+
+    let targets: Vec<DbTarget> = stmt_targets
+        .query_map([], |row| {
+            Ok(DbTarget {
+                name: row.get(0).unwrap(),
+                aspectRatio: row.get(1).unwrap(),
+            })
+        })
+        .unwrap()
+        .map(|res| res.unwrap())
+        .collect();
+
+    let mut stmt_random_prompt = conn.prepare("select prompts.id, prompts.prompt from promptTargets, prompts where prompts.id=promptTargets.promptId and promptTargets.targetName=?1 order by random() limit 1").unwrap();
+
+    let prompts: Vec<Prompt> = targets
+        .iter()
+        .map(|target| {
+            let db_prompt: DbPrompt = stmt_random_prompt
+                .query_row([&target.name], |row| {
+                    Ok(DbPrompt {
+                        id: row.get(0).unwrap(),
+                        prompt: row.get(1).unwrap(),
+                    })
+                })
+                .unwrap();
+            Prompt {
+                target: target.name.clone(),
+                prompt: db_prompt.prompt,
+            }
+        })
+        .collect();
+    Json(prompts)
 }
